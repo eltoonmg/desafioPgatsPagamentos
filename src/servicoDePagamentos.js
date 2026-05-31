0640f65b7bcab4485f03ab3e928de622c43a5b94
Elton Machado Gomes <elton5_@hotmail.com>
2026-05-31 20:19:48 -0300

    :white_check_mark: Inclusão dos Testes

---
+++ src/servicoDePagamentos.js
@@ -0,0 +1,51 @@
+const produtos = [
+  {
+    codigoBarras: '0987-7656-3475',
+    empresa: 'Mercado Livre',
+    valor: 55.50
+  },
+  {
+    codigoBarras: '0987-7656-3476',
+    empresa: 'Amazon',
+    valor: 2000.00
+  },
+  {
+    codigoBarras: '0987-7656-3477',
+    empresa: 'Shopee',
+    valor: 20.00
+  },
+  {
+    codigoBarras: '0987-7656-3478',
+    empresa: 'Americanas',
+    valor: 100.00
+  },
+  {
+    codigoBarras: '0987-7656-3479',
+    empresa: 'Americanas',
+    valor: 101.00
+  }
+];
+
+const pagamentos = []
+
+export function realizarPagamento(codigoBarras , empresa, valor) {
+
+    for (let produto of produtos) {    
+    if (produto.codigoBarras == codigoBarras) {
+      produto.categoria = produto.valor <= 100 ? 'padrao' : 'cara';
+
+      pagamentos.push(produto)
+      return pagamentos
+    }
+  }
+
+  throw new Error('Código de barras não encontrado');
+}
+
+export function consultarUltimoPagamento (){
+    if (pagamentos.length == 0) {
+        return "Nenhum pagamento realizado";
+    }
+
+  return pagamentos[pagamentos.length - 1];
+}
---
+++ test/servicoDePagamentos.test.js
@@ -0,0 +1,105 @@
+import { realizarPagamento , consultarUltimoPagamento } from "../src/servicoDePagamentos.js";
+import assert from 'node:assert';
+
+describe('Testes do Módulo de Pagamentos', function() {
+    it('Validar Ultimo Pagamento', function(){
+
+            //arrange (Dado que, pré-condições)
+            const codigoBarras = '0987-7656-3477';
+            const empresa = 'Shopee';
+            const valor = 20.00
+
+            //Act (Quando, ações)
+            const pagamento = realizarPagamento(codigoBarras, empresa, valor);
+            const consultarPagamento = consultarUltimoPagamento();
+
+            //Assert (Então, Pós-condições)
+            assert.equal(consultarPagamento.codigoBarras, '0987-7656-3477');
+            assert.equal(consultarPagamento.empresa, 'Shopee');
+            assert.equal(consultarPagamento.valor, 20.00);
+            assert.equal(consultarPagamento.categoria, 'padrao');
+        });
+
+        it('Validar Segundo Pagamento incluído Com Valor Padrão', function(){
+            //arrange (Dado que, pré-condições)
+            const codigoBarras = '0987-7656-3475';
+            const empresa = 'Mercado Livre';
+            const valor = 55.50
+
+            //Act (Quando, ações)
+            const pagamento = realizarPagamento(codigoBarras, empresa, valor);
+            const consultarPagamento = consultarUltimoPagamento();
+
+            //Assert (Então, Pós-condições)
+            assert.equal(consultarPagamento.codigoBarras, '0987-7656-3475');
+            assert.equal(consultarPagamento.empresa, 'Mercado Livre');
+            assert.equal(consultarPagamento.valor, 55.50);
+            assert.equal(consultarPagamento.categoria, 'padrao');
+        });
+
+        it('Validar terceiro Pagamento incluído Com Valor Cara', function(){
+            //arrange (Dado que, pré-condições)
+            const codigoBarras = '0987-7656-3476';
+            const empresa = 'Amazon';
+            const valor = 2000.00
+
+            //Act (Quando, ações)
+            const pagamento = realizarPagamento(codigoBarras, empresa, valor);
+            const consultarPagamento = consultarUltimoPagamento();
+
+            //Assert (Então, Pós-condições)
+            assert.equal(consultarPagamento.codigoBarras, '0987-7656-3476');
+            assert.equal(consultarPagamento.empresa, 'Amazon');
+            assert.equal(consultarPagamento.valor, 2000.00);
+            assert.equal(consultarPagamento.categoria, 'cara');
+        });
+
+        it('Validar Pagamento Com Valor Igual a 100.00', function(){
+            //arrange (Dado que, pré-condições)
+            const codigoBarras = '0987-7656-3478';
+            const empresa = 'Americanas';
+            const valor = 100.00
+
+            //Act (Quando, ações)
+            const pagamento = realizarPagamento(codigoBarras, empresa, valor);
+            const consultarPagamento = consultarUltimoPagamento();
+
+            //Assert (Então, Pós-condições)
+            assert.equal(consultarPagamento.codigoBarras, '0987-7656-3478');
+            assert.equal(consultarPagamento.empresa, 'Americanas');
+            assert.equal(consultarPagamento.valor, 100.00);
+            assert.equal(consultarPagamento.categoria, 'padrao');
+        });
+
+        it('Validar Pagamento Com Valor Igual a 101.00', function(){
+            //arrange (Dado que, pré-condições)
+            const codigoBarras = '0987-7656-3479';
+            const empresa = 'Americanas';
+            const valor = 101.00
+
+            //Act (Quando, ações)
+            const pagamento = realizarPagamento(codigoBarras, empresa, valor);
+            const consultarPagamento = consultarUltimoPagamento();
+
+            //Assert (Então, Pós-condições)
+            assert.equal(consultarPagamento.codigoBarras, '0987-7656-3479');
+            assert.equal(consultarPagamento.empresa, 'Americanas');
+            assert.equal(consultarPagamento.valor, 101.00);
+            assert.equal(consultarPagamento.categoria, 'cara');
+        });
+
+        it('Validar Pagamento Com Codigo De Barras Inexistente', function(){
+            //arrange (Dado que, pré-condições)
+            const codigoBarras = '0987-7656-3485';
+            const empresa = 'Americanas';
+            const valor = 101.00
+            
+            //Assert (Então, Pós-condições)
+            assert.throws(
+                () => realizarPagamento(codigoBarras, empresa, valor),
+                {
+                    message: 'Código de barras não encontrado'
+                }
+            );
+});
+});
